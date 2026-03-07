d1046cc1e94e60aa55c357286baea8270b3a2fe9
Engga <benggahdxml@gmail.com>
2026-03-08 01:20:50 +0700

    fitur baru

---
+++ backend/app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $request)
+    {
+        $product = Product::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'price' => $request->price
+        ]);
+
+        return response()->json($product);
+    }
+}
